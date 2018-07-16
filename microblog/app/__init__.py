b470e76473dde80b10e444fb964d85898380282d
Saswat Levin <saswatlevin@gmail.com>
2018-07-16 23:07:47 +0300

    Updated GUI using flask_bootstrap

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -7,10 +7,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from flask_mail import Mail
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 mail=Mail(app)
+bootstrap=Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
